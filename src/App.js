5582ced8de6810b449be228a7e5b3239da0d27b8
StepbyStepper <gorbunov.dv.webdev@gmail.com>
2018-12-03 03:28:26 +0300

    Создание метода, позволяющего работать с API

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import Weather from "./components/Weather";
 const API_KEY = "5ed3cb04a859a18d188597f16e0de054";
 
 class App extends React.Component {
+
+  gettingWeater = async () => {
+    const api_url = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
+    const data = await api_url.json();
+    console.log(data);
+  }
+
   render() {
     return (
       <div>
