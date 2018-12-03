5358ab57df44f8f56aa30ca7017601302763e51c
StepbyStepper <gorbunov.dv.webdev@gmail.com>
2018-12-03 03:20:38 +0300

    Создание компонентов Info и Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import Info from "./components/Info.js"
+import Info from "./components/Info";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "5ed3cb04a859a18d188597f16e0de054";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Info />
+        <Form />
+        <Weather />
       </div>
     );
   }
