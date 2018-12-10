116d0b7d57d3d2eb91f2818afaed9ce5ef4bc004
StepbyStepper <gorbunov.dv.webdev@gmail.com>
2018-12-10 14:00:18 +0300

    Добавление проверки на ошибки

--- src/App.js
+++ src/App.js
@@ -7,12 +7,33 @@ const API_KEY = "5ed3cb04a859a18d188597f16e0de054";
 
 class App extends React.Component {
 
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    sunrise: undefined,
+    sunset: undefined,
+    error: undefined
+  }
+
+
   gettingWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
-    const api_url = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
-    const data = await api_url.json();
-    console.log(data);
+
+    if(city) {
+      const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+      const data = await api_url.json();
+
+      this.setState({
+        temp: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        sunrise: data.sys.sunrise,
+        sunset: data.sys.sunset,
+        error: ""
+      });
+    }
   }
 
   render() {
@@ -20,7 +41,14 @@ class App extends React.Component {
       <div>
         <Info />
         <Form weatherMethod={this.gettingWeather} />
-        <Weather />
+        <Weather 
+          temp={this.state.temp}
+          city={this.state.city}
+          country={this.state.country}
+          sunrise={this.state.sunrise}
+          sunset={this.state.sunset}
+          error={this.state.error}
+        />
       </div>
     );
   }
